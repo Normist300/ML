5b521f1b090af7a0ef87294fb63a57307cf0b8eb
Normist <normist@yandex.ru>
2025-04-08 11:06:56 +0300

    continue

--- MIPT course/test.py
+++ MIPT course/test.py
@@ -1,2 +1,10 @@
-classes = tuple(str(i) for i in range(10))
-print(type(classes[0]))
+import torch
+import cv2
+import os
+
+print(os.path.abspath("sample_photo.jpeg"))
+
+img = cv2.imread("E:\Code\ML\sample_photo.jpeg")
+RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_t = torch.from_numpy(RGB_img).type(torch.float32).unsqueeze(0)
+img_t
